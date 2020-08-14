40191fca3e4f216e55cbfba53e4aacece50992b9
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-14 22:46:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,8 +6,56 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [{
-        
-      },{},{},{},{}]
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        rebounds: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      },{
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        rebounds: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      },{
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        rebounds: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      },{
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        rebounds: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      },{
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        rebounds: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+      }]
     },
     :away => {
       :team_name => "Charlotte Hornets",
